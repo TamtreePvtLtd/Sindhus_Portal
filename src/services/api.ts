219a4097f5a4f5dd3788a8f09b2a8ad962c27af6
kiruthikaSukumar <kiruthikasukumar@gmail.com>
2024-02-28 10:33:51 +0530

    specials added

--- src/services/api.ts
+++ src/services/api.ts
@@ -31,7 +31,7 @@ const getAllSpecials = async () => {
     const response = await httpWithoutCredentials.get<ISpecials>(
       "/specials/getAllSpecials"
     );
-
+// console.log("response",response.data.data)
     return response.data.data;
   } catch (error) {
     throw error;
