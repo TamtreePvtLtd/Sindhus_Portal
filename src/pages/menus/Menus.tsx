1ae49360a6b491121c0dcc7e28f138d86b817ba0
poongodimohan <poongodi.tamtree@gmail.com>
2024-03-06 17:41:28 +0530

    addimage

--- src/pages/menus/Menus.tsx
+++ src/pages/menus/Menus.tsx
@@ -6,34 +6,38 @@ import Fade from "react-reveal";
 import PageBanner from "../../common/component/pageBanner";
 
 const Menus = () => {
-    const { data: menus, isLoading: menusLoading, isError: menusError } = useGetAllMenus();
-    const [selectedMenuId, setSelectedMenuId] = useState<null | string>(null);
+    const [selectedMenuId, setSelectedMenuId] = useState<string>();
     const theme = useTheme();
     const [hoveredMenuId, setHoveredMenuId] = useState<null | string>(null);
 
-    const { data: productsData, isLoading: productsLoading, isError: productsError, refetch: refetchProducts } = useGetFetchProductsByMenuId(
-        selectedMenuId || ''
-    );
+    const { data: menus, isLoading: menusLoading, isError: menusError } = useGetAllMenus();
 
     const handleMenuClick = (menuId: string) => {
         setSelectedMenuId(menuId);
     };
-
-    useEffect(() => {
-        if (selectedMenuId) {
-            refetchProducts();
-        }
-    }, [selectedMenuId, refetchProducts]);
 
     const getMenuItemsInAlphabeticalOrder = () => {
         return menus ? [...menus].sort((a, b) => a.title.localeCompare(b.title)) : [];
     };
 
     useEffect(() => {
-        const appetizersMenuId = getMenuItemsInAlphabeticalOrder().find(menu => menu.title === 'Appetizers')?._id;
-        setSelectedMenuId(appetizersMenuId || null);
+        if (!selectedMenuId && menus && menus.length > 0) {
+            const firstMenuItemId = getMenuItemsInAlphabeticalOrder();
+            console.log(firstMenuItemId[0]);
+
+            setSelectedMenuId(firstMenuItemId[0]._id);
+            refetchProducts()
+        }
     }, [menus]);
 
+
+    useEffect(() => {
+        refetchProducts()
+    }, [selectedMenuId])
+
+    const { data: productsData, refetch: refetchProducts } = useGetFetchProductsByMenuId(
+        selectedMenuId!
+    );
     return (
         <div>
             <Box>
@@ -54,7 +58,7 @@ const Menus = () => {
                         }}>
 
                             {getMenuItemsInAlphabeticalOrder().map((menu,) => (
-                                <Grid item key={menu._id} columnGap={4} xs={12} sm={6} md={3} lg={'auto'} >
+                                <Grid item key={menu._id} columnGap={4} xs={'auto'} sm={3} md={3} lg={'auto'} >
                                     <Box
                                         sx={{
                                             gap: 4,
@@ -63,10 +67,8 @@ const Menus = () => {
                                             paddingX: '20px',
                                             cursor: 'pointer',
                                             color: (selectedMenuId === menu._id || hoveredMenuId === menu._id) ? 'text.primary' : 'text.disabled',
-                                            textDecoration: (selectedMenuId === menu._id || hoveredMenuId === menu._id) ? 'underline' : 'none',
                                             '&:hover': {
-                                                color: 'text.primary',
-                                                textDecoration: 'underline',
+                                                borderBottom: `1.5px solid #038265`,
                                             },
                                         }}
                                         onClick={() => handleMenuClick(menu._id)}
@@ -76,7 +78,6 @@ const Menus = () => {
                                         <Box>
                                             <Fade left>
                                                 <Typography style={{
-                                                    margin: 0,
                                                     lineHeight: '2',
                                                     marginBottom: '10px',
                                                     fontFamily: 'revert-layer',
@@ -93,47 +94,73 @@ const Menus = () => {
                             ))}
                         </Grid>
                     )}
-                    <Divider sx={{ marginTop: '50px' }} />
-                    {selectedMenuId && (
-                        <Card sx={{ maxWidth: 700, margin: 'auto', boxShadow: 'none' }}>
-                            <CardContent>
-                                <Typography variant="h4" gutterBottom style={{ color: theme.palette.primary.main, fontFamily: '"Lucida Handwriting", cursive', fontWeight: "bold" }}>
-                                    {menus?.find((m) => m._id === selectedMenuId)?.title}
-                                </Typography>
-                                {productsLoading && <p>Loading products...</p>}
-                                {productsError && <p>Error fetching products</p>}
 
-                                {productsData && productsData.products && (
-                                    <Grid container spacing={2}>
-                                        {productsData.products.map((product) => (
-                                            <Grid item key={product._id} xs={12} sm={12} md={12} lg={12}>
-                                                <Fade left>
-                                                    <Grid sx={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center' }}>
-                                                        <Grid item xs={8}>
-                                                            <Box>
-                                                                <Typography variant="h6">{product.title}</Typography>
-                                                                <Typography>{product.description}</Typography>
-                                                            </Box>
-                                                        </Grid>
-                                                        <Grid item xs={1} sx={{ textAlign: 'center' }}>
-                                                            <ArrowRightIcon sx={{ color: theme.palette.primary.main, }} />
-                                                        </Grid>
-                                                        <Grid item xs={3} style={{ textAlign: 'center' }}>
-                                                            <Typography variant="h6">
-                                                                {product.dailyMenuSizeWithPrice && product.dailyMenuSizeWithPrice.length > 0
-                                                                    ? `$${product.dailyMenuSizeWithPrice[0].price.toFixed(2)}`
-                                                                    : ''}
-                                                            </Typography>
-                                                        </Grid>
+                    <Divider sx={{ marginTop: '50px' }} />
+
+
+                    {selectedMenuId && (
+                        <Grid container spacing={2}>
+                            <Grid item xs={8}>
+                                <Card sx={{ maxWidth: 700, margin: 'auto', boxShadow: 'none' }}>
+                                    <CardContent>
+                                        <Typography variant="h4" gutterBottom style={{ color: theme.palette.primary.main, fontFamily: '"Lucida Handwriting", cursive', fontWeight: "bold" }}>
+                                            {menus?.find((m) => m._id === selectedMenuId)?.title}
+                                        </Typography>
+                                        {productsData && productsData.products && (
+                                            <Grid container spacing={2}>
+                                                {productsData.products.map((product) => (
+                                                    <Grid item key={product._id} xs={12} sm={12} md={12} lg={12}>
+                                                        <Fade left>
+                                                            <Grid sx={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                                                <Grid item xs={8}>
+                                                                    <Box>
+                                                                        <Typography variant="h6">{product.title}</Typography>
+                                                                        <Typography>{product.description}</Typography>
+                                                                    </Box>
+                                                                </Grid>
+                                                                <Grid item xs={1} sx={{ textAlign: 'center' }}>
+                                                                    <ArrowRightIcon sx={{ color: theme.palette.primary.main, }} />
+                                                                </Grid>
+                                                                <Grid item xs={3} style={{ textAlign: 'center' }}>
+                                                                    <Typography variant="h6">
+                                                                        {product.dailyMenuSizeWithPrice && product.dailyMenuSizeWithPrice.length > 0
+                                                                            ? `$${product.dailyMenuSizeWithPrice[0].price.toFixed(2)}`
+                                                                            : ''}
+                                                                    </Typography>
+                                                                </Grid>
+                                                            </Grid>
+                                                        </Fade>
                                                     </Grid>
-                                                </Fade>
+                                                ))}
                                             </Grid>
-                                        ))}
-                                    </Grid>
-                                )}
-                                <Divider sx={{ marginTop: '20px' }} />
-                            </CardContent>
-                        </Card>
+                                        )}
+                                        {/* <Divider sx={{ marginTop: '20px' }} /> */}
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Fade right>
+                                    <Box sx={{
+                                        position: 'relative',
+                                        height: '100%',
+                                        textAlign: 'center',
+                                        overflow: 'hidden',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        marginTop: 8,
+                                        // borderRadius: '70px 70px 70px 70px',
+                                    }}>
+                                        <img
+                                            src='public\assets\SINDHU-KITCHEN-samosa.webp'
+                                            alt={`Image`}
+                                            style={{ maxWidth: '100%', margin: '5px' }}
+                                        />
+
+                                    </Box>
+                                </Fade>
+                            </Grid>
+                        </Grid>
+
                     )}
                 </Box>
             </Container>
