5c89658ffe24cb547d9d239cbc9561b85fdc5d90
Logeswaran <logeswaran2108@gmail.com>
2024-03-22 11:09:41 +0530

    google review

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,13 +3,58 @@ import HomePageSlicker from "./HomePageSlicker";
 import Box from "@mui/material/Box";
 import { WhyChooseUsData, ourServicesData } from "../../seed-data/seed-data";
 import WhyChooseUs from "./WhyChooseUs";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import GoogleReviewsModal from "./GoogleReview";
 
 function Home() {
+
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenReviewModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseReviewModal = () => {
+    setOpenModal(false);
+  };
+
+ 
+  
   return (
     <>
+    <Box>
+        <Box>
+          <Button
+            variant="contained"
+            onClick={handleOpenReviewModal}
+            sx={{
+              position: "fixed",
+              top: 200,
+              left: 0,
+              margin: "-50px",
+              height: "30px",
+              cursor: "pointer",
+              justifyContent: "space-between",
+              alignItems: "center",
+              zIndex: 99,
+              bgcolor:"#f5c03d",
+              display: "flex",
+              transform: "rotate(-90deg) translate(-50%, 50%)",
+              "&:hover": {
+                backgroundColor: "#f5c03d",
+              },
+            }}
+          >
+            Reviews
+          </Button>
+          
+        </Box>
+      </Box>
       <Box>
         <HomePageSlicker />
       </Box>
+      
       <Box
         sx={{
           mb: 3,
@@ -20,6 +65,8 @@ function Home() {
       {/* <Box sx={{ backgroundColor: "#e6e7ee", py: 4 }}>
         <OurServices OurServices={ourServicesData} />
       </Box> */}
+
+<GoogleReviewsModal open={openModal} onClose={handleCloseReviewModal} />
     </>
   );
 }
