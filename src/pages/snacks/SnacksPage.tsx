235b3c7c0bece61bb04fa314398a3b5928304a8f
Deepa-Senthil <deepasenthil5812@gmail.com>
2024-10-14 13:39:43 +0530

    Banner image fit for all views

--- src/pages/snacks/SnacksPage.tsx
+++ src/pages/snacks/SnacksPage.tsx
@@ -6,7 +6,6 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import CommonSnacksCard from "../../common/component/CommonSnacksCard";
-import { useMediaQuery, useTheme } from "@mui/material";
 
 function SnacksPage() {
   const [selectedSubMenuId, setSelectedSubMenuId] = useState<string>("");
@@ -21,52 +20,12 @@ function SnacksPage() {
   const handleSubMenuClick = (subMenuId: string) => {
     setSelectedSubMenuId(subMenuId);
   };
- const theme = useTheme();
- const isSmallScreen = useMediaQuery("(max-width:600px)");
- const isMediumScreen = useMediaQuery(
-   "(min-width:601px) and (max-width:1500px)"
- );
- const isLargeScreen = useMediaQuery("(min-width:961px)");
 
- const getHeight = () => {
-   if (isSmallScreen) return "250px";
-   if (isMediumScreen) return "410px";
-   if (isLargeScreen) return "125vh";
-  };
-   const getWidth = () => {
-     if (isSmallScreen) return "100%";
-     if (isMediumScreen) return "100%";
-     if (isLargeScreen) return "100vw";
-  };
-  // const getWidth = () => {
-  //   return "100vw"; // Full viewport width for all screen sizes
-  // };
-    const getBackgroundSize = () => {
-      if (isSmallScreen || isMediumScreen) return "contain";
-      return "cover";
-    };
   return (
     <>
-      {/* <Box
-        sx={{
-          backgroundImage: `url("public/assets/snacks-banner-2.jpg")`,
-          backgroundSize: getBackgroundSize(),
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          width: getWidth(),
-          height: getHeight(),
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          // margin: "0 auto",
-          zIndex: 99,
-        }}
-      >
-      </Box> */}
       <Grid
         style={{
           width: "100%",
-          // height: "500px",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
